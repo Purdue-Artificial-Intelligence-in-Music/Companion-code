dfcafe8dfecf560819d7e6c8fda572aed1c8c93d
unknown <timmynadolsky@gmail.com>
2023-09-24 16:11:09 -0400

    Initial commit

---
+++ src/AudioThread.py
@@ -0,0 +1,94 @@
+import pyaudio
+import numpy as np
+import threading
+import time
+
+'''
+This class is a template class for a thread that reads in audio from PyAudio.
+'''
+
+class AudioThread(threading.Thread):
+    def __init__(self, name, starting_chunk_size, process_func, args_before, args_after):
+        """
+        Initializes an AudioThread.
+        Parameters:
+            name: the name of the thread
+            starting_chunk_size: an integer representing the chunk size in samples
+            process_func: the function to be called as a callback when new audio is received from PyAudio
+            args_before: a tuple of arguments for process_func to be put before the sound array
+            args_after: a tuple of arguments for process_func to be put after the sound array
+        Returns: nothing
+        """
+        super(AudioThread, self).__init__()
+        self.name = name    # General imports
+        self.process_func = process_func
+        self.args_before = args_before
+        self.args_after = args_after
+        
+        self.p = None    # PyAudio vals
+        self.stream = None
+        self.FORMAT = pyaudio.paFloat32
+        self.CHANNELS = 2
+        self.RATE = 44100
+        self.CHUNK = starting_chunk_size * 2
+
+        self.stop_request = False
+        
+        self.data = None
+        
+    def set_args_before(a):
+        """
+        Changes the arguments before the sound array when process_func is called.
+        Parameters: a: the arguments
+        Returns: nothing
+        """
+        self.args_before = a
+    
+    def set_args_after(a):
+        """
+        Changes the arguments after the sound array when process_func is called.
+        Parameters: a: the arguments
+        Returns: nothing
+        """
+        self.args_after = a
+    
+    def run(self):
+        """
+        When the thread is started, this function is called which opens the PyAudio object
+        and keeps the thread alive.
+        Parameters: nothing
+        Returns: nothing
+        """
+        self.p = pyaudio.PyAudio()
+        self.stream = self.p.open(format=self.FORMAT,
+                                  channels=self.CHANNELS,
+                                  rate=self.RATE,
+                                  input=True,
+                                  output=False,
+                                  stream_callback=self.callback,
+                                  frames_per_buffer=self.CHUNK)
+        while not self.stop_request:
+            time.sleep(1.0)
+        self.stop()
+            
+    def stop(self):
+        """
+        When the thread is stopped, this function is called which closes the PyAudio object
+        Parameters: nothing
+        Returns: nothing
+        """
+        self.stream.stop_stream()
+        self.stream.close()
+        self.p.terminate()
+
+    def callback(self, in_data, frame_count, time_info, flag):
+        """
+        This function is called whenever PyAudio recieves new audio. It calls process_func to process the sound data
+        and stores the result in the field "data".
+        This function should never be called directly.
+        Parameters: none user-exposed
+        Returns: nothing of importance to the user
+        """
+        numpy_array = np.frombuffer(in_data, dtype=np.float32)
+        self.data = self.process_func(*self.args_before, numpy_array, *self.args_after)
+        return None, pyaudio.paContinue
---
+++ src/threaded_parent.py
@@ -0,0 +1,24 @@
+from AudioThread import AudioThread
+import time
+
+STARTING_CHUNK = 1024
+
+def process_func(audio):
+    # Do something cool here!
+    return
+
+def main():
+    try:
+        AThread = AudioThread(name = "SPA_Thread", starting_chunk_size=STARTING_CHUNK, process_func=process_func)
+        print("All threads init'ed")
+        AThread.start()
+        print("============== AThread started")
+        while True:
+            # Do something here!
+            time.sleep(0.1)
+    except KeyboardInterrupt:
+        AThread.stop_request = True
+        
+
+if __name__ == "__main__":
+    main()
