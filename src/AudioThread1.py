6b1126c110f6c21857e5729662ef90c3f68999fb
GuanxiZhou <107327693+GuanxiZhou@users.noreply.github.com>
2023-10-23 03:04:39 -0400

    Update AudioThread1.py

--- src/AudioThread1.py
+++ src/AudioThread1.py
@@ -7,7 +7,6 @@ import time
 This class is a template class for a thread that reads in audio from PyAudio.
 '''
 
-
 class AudioThread(threading.Thread):
     def __init__(self, name, starting_chunk_size, process_func, args_before=(), args_after=()):
         """
