939b7168f457c7351bfc21ab1dd2feaa88439233
GuanxiZhou <107327693+GuanxiZhou@users.noreply.github.com>
2023-10-23 14:59:24 -0400

    Update threaded_parent-new.py

--- src/threaded_parent-new.py
+++ src/threaded_parent-new.py
@@ -4,10 +4,11 @@ import numpy as np
 
 STARTING_CHUNK = 1024
 
-def process_func(self, data2):
-    data = np.copy(data2).astype(np.float64, casting='safe')
-    # Does nothing
-        # input to buffer
+def process_func(self, mic_data, wav_data):
+    data = np.copy(mic_data).astype(np.float64, casting='safe')
+    wav_data = wav_data.astype(np.float64)
+    
+    # input to buffer
     if self.buffer_index + len(data) <= self.buffer_size:
         self.audio_buffer[self.buffer_index:self.buffer_index + len(data)] = data
         self.buffer_index += len(data)
@@ -22,17 +23,18 @@ def process_func(self, data2):
     # use RMS to match the input amplitude and output amplitude
     if data is not None:
         input_amplitude = np.sqrt(np.mean(data ** 2))
-        output_amplitude = np.sqrt(np.mean(data ** 2))
+        output_amplitude = np.sqrt(np.mean(wav_data ** 2))
 
         scaling_factor = input_amplitude / (output_amplitude + 1e-6)
-        data *= scaling_factor
+        wav_data *= scaling_factor
     else:
         data_bytes = b''
-    return data.astype(np.int16)
+    return wav_data.astype(np.int16)
 
 
 def main():
-    AThread = AudioThread(name="SPA_Thread", starting_chunk_size=STARTING_CHUNK, process_func=process_func)
+    
+    AThread = AudioThread(name="SPA_Thread", starting_chunk_size=STARTING_CHUNK, process_func=process_func, wav_file="2.wav")
     print("All threads init'ed")
     try:
         AThread.start()
