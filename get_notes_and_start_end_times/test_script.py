75253e1e53aeca0cdf1a2a3787713b4677bb164d
Ping-Hung Nick Ko <ko109@purdue.edu>
2024-02-17 15:08:05 -0500

    --amend

--- get_notes_and_start_end_times/test_script.py
+++ get_notes_and_start_end_times/test_script.py
@@ -1,4 +1,4 @@
-import get_notes_and_start_end_times as parse
+import parse_score as ps
 from music21 import *
 
 if __name__ == '__main__':
@@ -11,13 +11,13 @@ if __name__ == '__main__':
         print(part)
 
     print(f"the piece is in the key of {score.analyze('key')}")
-    n_measures = parse.find_num_measures_in_score(score=score)
+    n_measures = ps.find_num_measures_in_score(score=score)
     print(f'the piece has {n_measures} measures')
 
     """this part looks a little off, but may be resolved by fixing the function 
     collect_all_notes_from_score:
         just find number of measures for only a part, as other parts should play in harmony"""
-    all_notes_set = parse.collect_all_notes_from_score(score)
+    all_notes_set = ps.collect_all_notes_from_score(score)
 
     print(f"there are {len(all_notes_set)} measures in the piece, which contains notes: ")
     for i,s in enumerate(all_notes_set, start=1):
@@ -26,7 +26,7 @@ if __name__ == '__main__':
             print(f'\t{n}')
 
     flatten_notes_set = set()
-    parse.flatten_frozenset_in_sets(all_notes_set, flatten_notes_set)
+    ps.flatten_frozenset_in_sets(all_notes_set, flatten_notes_set)
     print(f'there are {len(flatten_notes_set)} notes involved with the piece, namely: ')
     for i,n in enumerate(flatten_notes_set, start=1):
         print(f"\tnote #{i}: {n}: frequency {n.pitch.frequency:.4}")
@@ -42,7 +42,7 @@ if __name__ == '__main__':
     print(n1 is n2)
 
     #testing turning into frequency
-    freq_list = parse.translate_note_to_freq(flatten_notes_set)
+    freq_list = ps.translate_note_to_freq(flatten_notes_set)
     #test if length are the same
     assert len(freq_list) == len(flatten_notes_set), \
     f"freq_list has lenth {len(freq_list)}; flatten_notes_set has lenth {len(flatten_notes_set)}"
