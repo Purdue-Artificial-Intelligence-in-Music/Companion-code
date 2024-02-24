c8f6e8505de47a40696d24233130aeae2085ce0a
Ping-Hung Nick Ko <ko109@purdue.edu>
2024-02-24 16:23:39 -0500

    first attempt on parse_audio

--- get_notes_and_start_end_times/parse_audio.py
+++ get_notes_and_start_end_times/parse_audio.py
@@ -10,5 +10,14 @@ def set_sampling_rate(sr: int) -> None:
     SAMPLING_RATE = sr
 
 def get_start_end_time_of_notes(freq_keys: list[float], audio: np.array) -> dict[float : tuple[int, int]]:
-    
-    return None
+    # note that freq_keys.length <= audio.length
+    freq_to_time = {freq: [0,0] for freq in freq_keys}
+    # try to account for repeated notes in a section (e.g. if an audio plays 4 C notes in a single measure, make sure to get the correct start end times for each C)
+    for idx, sample in enumerate(audio):
+        freq_to_time[sample][0] = idx / SAMPLING_RATE
+        freq_to_time[sample][1] = (idx + 1)/ SAMPLING_RATE
+
+    for freq in freq_to_time.keys():
+        freq_to_time[freq] = tuple(freq_to_time[freq])
+
+    return freq_to_time
