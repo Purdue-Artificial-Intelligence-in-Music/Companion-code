912e350866593a00113b94346048352b6b5bd85f
Ping-Hung Nick Ko <ko109@purdue.edu>
2024-03-20 11:45:02 -0400

    writing test script

--- get_notes_and_start_end_times/test_parse_audio.py
+++ get_notes_and_start_end_times/test_parse_audio.py
@@ -14,10 +14,11 @@ if __name__ == '__main__':
     score = converter.parse('Abide by Me.midi')
     all_notes_set = collect_all_notes_from_score(score)
     flatten_notes_set = set()
-    flatten_frozenset_in_sets(all_notes_set, flatten_notes_set)
+    flatten_frozenset_in_sets(all_notes_set, flatten_notes_set) #has all note names
+    freq_list : list[float] = translate_note_to_freq(flatten_frozenset_in_sets)
 
     # parse audio
     set_sampling_rate(sr=sr)
-    
+    freq_to_time : dict[float:tuple[float, float]] = get_start_end_time_of_notes(freq_keys=freq_list,audio=audio)
 
     # log parsed result onto a separate file
