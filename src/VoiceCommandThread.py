9203d24538f640a79667b79afc765a8b2466ea05
EddieN03 <eddiefalcon20@gmail.com>
2024-06-24 13:50:06 -0400

    Update VoiceCommandThread.py
    VoiceCommandingThread is now more aligned with the vosk testing that I have been doing, I need to debug this more but it was working at least without attaching to the main program before.

--- src/VoiceCommandThread.py
+++ src/VoiceCommandThread.py
@@ -7,6 +7,14 @@ import numpy as np
 from nltk.stem import PorterStemmer
 from transformers import pipeline
 
+import argparse
+import queue
+import sys
+import sounddevice as sd
+import json
+
+from vosk import Model, KaldiRecognizer
+
 
 class VoiceAnalyzerThread(threading.Thread):
     def __init__(self, name: str, BUFFER: AudioBuffer, voice_length=3):
@@ -20,6 +28,16 @@ class VoiceAnalyzerThread(threading.Thread):
         self.voice_length = voice_length
         # Initialize whatever stuff you need here
 
+        #The following are all related to the use of Vosk and voice commanding
+        self.q = queue.Queue()
+
+        # Define the list of possible commands along with hypothesis templates
+        self.commands = {
+            "stop": "The action involves bringing something to a halt.",
+            "start": "The action involves beginning something.",
+            "exit": "The action involves the termination of something.",
+        }
+
         # Initialize the zero-shot classification pipeline with the Roberta model
         self.classifier = pipeline('zero-shot-classification', model='roberta-large-mnli')
 
@@ -28,6 +46,94 @@ class VoiceAnalyzerThread(threading.Thread):
         return sr.AudioData(frame_data=arr.tobytes(), 
                             sample_rate=self.BUFFER.RATE, 
                             sample_width=4)
+    
+    def int_or_str(text):
+    #Helper function for argument parsing.
+        try:
+            return int(text)
+        except ValueError:
+            return text
+    
+    def callback(self, indata, frames, time, status):
+    #This is called (from a separate thread) for each audio block.
+        if status:
+            print(status, file=sys.stderr)
+        self.q.put(bytes(indata))
+
+    # Enhance the classify_command function to handle a wider range of negations
+    def classify_command(self, user_input):
+        # Check for negations and adjust the command accordingly
+        negations = ["don't", "do not", "please don't", "never", "no", "not"]
+        # Adjustments for negations
+        adjustments = {
+            "don't stop": "start",
+            "do not stop": "start",
+            "never stop": "start",
+            "don't go": "stop",
+            "do not go": "stop",
+            "never go": "stop",
+        }
+        # Action phrases that directly map to commands
+        action_phrases = {
+            "too fast": "slow down",
+            "too slow": "speed up",
+            "too loud": "volume down",
+            "too quiet": "volume up",
+            "go": "start",
+            "halt": "stop",
+            "be quiet": "volume down",
+            "stop playing": "stop",  
+            "pause": "stop", 
+            "I can't hear": "volume up",
+            "it's noisy": "volume down",
+        }
+
+        # First check for any negation adjustments
+        for phrase, command in adjustments.items():
+            if phrase in user_input.lower():
+                return command
+
+        # Next, check for direct action phrase mappings
+        for phrase, command in action_phrases.items():
+            if phrase in user_input.lower():
+                return command
+
+        # If no special cases, proceed with model classification
+        hypotheses = [f"The action is: {desc}" for desc in self.commands.values()]
+        result = self.classifier(user_input, hypotheses, multi_label=True) # Changed multi_class to multi_label
+        highest_score = max(result['scores'])
+        highest_scoring_command = [cmd for cmd, desc in self.commands.items() if f"The action is: {desc}" == result['labels'][result['scores'].index(highest_score)]][0]
+        return highest_scoring_command
+    
+    def run_command(self, command):
+
+        if command == "start":
+
+            #Check if audio is playing
+            self.BUFFER.audio_on()
+            #If audio is not playing from the AudioBuffer, then start playing audio
+                            
+            if self.BUFFER.input_on() is False:
+                    print("Turning on audio")
+                    self.BUFFER.unpause()
+
+        elif command == "stop":
+
+            print("stop")
+            #Still run Buffer in the background, but don't play audio
+            self.BUFFER.pause()
+
+        elif command == "exit":
+
+            print("Detected interrupt")
+            #Stop both voice and buffer
+            self.stop_request = True
+            self.BUFFER.stop()
+
+        else:
+            #TODO
+            print("Run the user's defined command")
+
 
     # Function to convert text to
     # speech
@@ -45,101 +151,7 @@ class VoiceAnalyzerThread(threading.Thread):
             MyText = self.r.recognize_google(audio)
             MyText = MyText.lower()
             spokenWords = np.array(MyText.split())
-            # Define the list of possible commands along with hypothesis templates
-            commands = {
-                "speed up": "The action involves increasing speed.",
-                "slow down": "The action involves decreasing speed.",
-                "volume up": "The action involves increasing volume.",
-                "volume down": "The action involves decreasing volume.",
-                "stop": "The action involves bringing something to a halt.",
-                "start": "The action involves beginning something.",
-                "exit": "The action involves the termination of something.",
-                # "edit": "The action involves changing something."
-            }
-
-            # Enhance the classify_command function to handle a wider range of negations
-            # Check for negations and adjust the command accordingly
-            negations = ["don't", "do not", "please don't", "never", "no", "not"]
-            # Adjustments for negations
-            adjustments = {
-                "don't stop": "start",
-                "do not stop": "start",
-                "never stop": "start",
-                "don't go": "stop",
-                "do not go": "stop",
-                "never go": "stop",
-                # "don't do this": "edit",
-                # "do not do this": "edit",
-                # "never do this": "edit",
-            }
-            # Action phrases that directly map to commands
-            action_phrases = {
-                "too fast": "slow down",
-                "too slow": "speed up",
-                "too loud": "volume down",
-                "too quiet": "volume up",
-                "go": "start",
-                "halt": "stop",
-                "be quiet": "volume down",
-                "stop playing": "stop",  
-                "pause": "stop", 
-                "I can't hear": "volume up",
-                "it's noisy": "volume down",
-                # "fix": "edit",
-                # "change": "edit",
-                # "alter": "edit",
-                # "modify": "edit",
-            }
-
-            # #Preset to "edit" since that's the key to adding/changing a command
-            # myCommand = "edit"
-            # toInsert = True
             
-            # First check for any negation adjustments
-            for phrase, command in adjustments.items():
-                if phrase in spokenWords.lower():
-                    command = command
-
-            # Next, check for direct action phrase mappings
-            for phrase, command in action_phrases.items():
-                if phrase in spokenWords.lower():
-                    command = command
-
-            # #If no new command was grabbed then act upon the edit
-            # if (myCommand == "edit"):
-            #     print("No command found for what you said, please define your command")
-                
-            #     name = spokenWords
-
-            #     MyText = self.r.recognize_google(audio)
-            #     MyText = MyText.lower()
-            #     yourDesc = np.array(MyText.split())
-
-            #     # First check if this is just a new negation
-            #     for phrase, command in adjustments.items():
-            #         if yourDesc in spokenWords.lower():
-            #             adjustments[name] = yourDesc
-            #             toInsert = False
-
-            #     # Next, check if its a new alterantive mapping
-            #     for phrase, command in action_phrases.items():
-            #         if yourDesc in spokenWords.lower():
-            #             action_phrases[name] = yourDesc
-            #             toInsert = False
-
-            #     # Lastly, if it wasn't either then insert it properly into the commands list
-            #     if (toInsert):
-            #         commands[name] = yourDesc
-
-
-            # If no special cases, proceed with model classification
-            hypotheses = [f"The action is: {desc}" for desc in commands.values()]
-            result = self.classifier(spokenWords, hypotheses, multi_label=True) # Changed multi_class to multi_label
-            highest_score = max(result['scores'])
-            highest_scoring_command = [cmd for cmd, desc in commands.items() if f"The action is: {desc}" == result['labels'][result['scores'].index(highest_score)]][0]
-
-            # Now that the most optimal command has been determined... 
-            self.output = highest_scoring_command
 
             # TODO: Process the command and act upon the AudioBuffer
             # MVP: Get start and stop to work
@@ -164,7 +176,7 @@ class VoiceAnalyzerThread(threading.Thread):
 
                 print("Detected interrupt")
                 #Stop both voice and buffer
-                self.stop()
+                self.stop_request = True
                 self.BUFFER.stop()
 
             else:
@@ -184,11 +196,92 @@ class VoiceAnalyzerThread(threading.Thread):
 
     def run(self):
         time.sleep(0.5)
-        while not self.stop_request:
-            smp = self.BUFFER.get_last_samples(self.BUFFER.RATE * self.voice_length)
-            if len(smp) >= self.BUFFER.RATE * self.voice_length / 2.0:
-                self.getSpeech(self.convert_to_AudioData(smp))
-            time.sleep(self.voice_length)
+
+        parser = argparse.ArgumentParser(add_help=False)
+        parser.add_argument(
+            "-l", "--list-devices", action="store_true",
+            help="show list of audio devices and exit")
+        args, remaining = parser.parse_known_args()
+        if args.list_devices:
+            print(sd.query_devices())
+            parser.exit(0)
+        parser = argparse.ArgumentParser(
+            description=__doc__,
+            formatter_class=argparse.RawDescriptionHelpFormatter,
+            parents=[parser])
+        parser.add_argument(
+            "-f", "--filename", type=str, metavar="FILENAME",
+            help="audio file to store recording to")
+        parser.add_argument(
+            "-d", "--device", type=self.int_or_str,
+            help="input device (numeric ID or substring)")
+        parser.add_argument(
+            "-r", "--samplerate", type=int, help="sampling rate")
+        parser.add_argument(
+            "-m", "--model", type=str, help="language model; e.g. en-us, fr, nl; default is en-us")
+        args = parser.parse_args(remaining)
+
+        try:
+            if args.samplerate is None:
+                device_info = sd.query_devices(args.device, "input")
+                # soundfile expects an int, sounddevice provides a float:
+                args.samplerate = int(device_info["default_samplerate"])
+                
+            if args.model is None:
+                model = Model(model_name="vosk-model-en-us-0.22-lgraph")
+            else:
+                model = Model(lang=args.model)
+
+            if args.filename:
+                dump_fn = open(args.filename, "wb")
+            else:
+                dump_fn = None
+
+            with sd.RawInputStream(samplerate=args.samplerate, blocksize = 8000, device=args.device,
+                    dtype="int16", channels=1, callback=self.callback):
+                print("#" * 80)
+                print("Press Ctrl+C or say exit to stop the recording")
+                print("#" * 80)
+
+                rec = KaldiRecognizer(model, args.samplerate)
+                while not self.stop_request:
+                    data = self.q.get()
+                    if rec.AcceptWaveform(data):
+                        #Print the result
+                        print(rec.Result())
+
+                        #Parse the json text and find the command
+                        res = json.loads(rec.Result())
+                        print(res['text'])
+                        if res['text'] != "":
+                            #print(toParse)                
+                            toParse = self.classify_command(res['text'])
+                            self.run_command(toParse)
+
+                    else:
+                        print(rec.PartialResult())
+                        #print()
+
+                        #Possible optimization by using partials rather than whole
+                        #Parse the json text and find the command
+                        res = json.loads(rec.PartialResult())
+                        print(res["partial"])
+                        if res["partial"] != "":
+                            #print(toParse)
+                            toParse = self.classify_command(res["partial"])
+                            self.run_command(toParse)
+
+                    if dump_fn is not None:
+                        dump_fn.write(data)
+
+        except KeyboardInterrupt:
+            print("\nDone")
+            self.stop_request = True
+            self.BUFFER.stop()
+            parser.exit(0)
+            
+        except Exception as e:
+            parser.exit(type(e).__name__ + ": " + str(e))
 
 def main():
     #Todo: Change to your file path
