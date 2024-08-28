de065bb8a80719f9c55abf256a5b503dc3f09721
Ryan Jordan <34822768+rtjord@users.noreply.github.com>
2024-08-28 17:00:52 -0400

    Modified requirements.txt and setup.py to resolve dependency conflicts. (#42)
    * removed unused files from src
    
    * modified setup.py and requirements.txt to reduce dependencies

--- setup.py
+++ setup.py
@@ -1,12 +1,6 @@
 from subprocess import run
 
-with open('requirements.txt') as f:
-    packages = f.readlines()
-    packages = [name.strip() for name in packages]
+run(['pip', 'install', '-r', 'requirements.txt'])
 
-for package in packages:
-    run(['pip', 'install', package])
-
-run(['pip', 'install', 'torch', 'torchvision', 'torchaudio', '--index-url', 'https://download.pytorch.org/whl/cu121'])
-
-run(['midi_ddsp_download_model_weights'])
+# run(['pip', 'install', 'torch', 'torchvision', 'torchaudio', '--index-url', 'https://download.pytorch.org/whl/cu121'])
+# run(['midi_ddsp_download_model_weights'])
