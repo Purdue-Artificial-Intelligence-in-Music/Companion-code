e2fb469b9406d3047bdcb54fef3a460a678876a7
Ping-Hung Nick Ko <ko109@purdue.edu>
2024-09-03 14:47:36 -0400

    create test file

--- module_tests/AudioBuffer_test.py
+++ module_tests/AudioBuffer_test.py
@@ -1,5 +1,14 @@
+import sys
+import os
+
+# importing AudioBuffer to this script
+# Add the directory to sys.path
+module_path = os.path.abspath(os.path.join('..', 'src/AudioBuffer.py'))
+if module_path not in sys.path:
+    sys.path.append(module_path)
+import AudioBuffer
+
 import unittest
-from ../src/AudioBuffer import *
 
 class AudioBufferTests(unittest.TestCase):
     def test_AudioBuffer_constructor():
