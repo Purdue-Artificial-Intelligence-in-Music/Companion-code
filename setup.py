4190724580251951e231f280012ab456f8a96f4d
Ryan Jordan <34822768+rtjord@users.noreply.github.com>
2024-09-11 16:38:33 -0400

    replaced MIDI-DDSP with FluidSynth

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from subprocess import run
 import os
 import requests
+import zipfile
 
 
 run(['pip', 'install', '-r', 'requirements.txt'])
@@ -11,9 +12,17 @@ response = requests.get(url)
 if response.status_code == 200:
     if not os.path.exists('soundfonts'):
         os.mkdir('soundfonts')
-    path = os.path.join('soundfonts', 'soundfontsFluidR3_GM.zip')
-    with open(path, 'wb') as file:
+    zip_path = os.path.join('soundfonts', 'soundfontsFluidR3_GM.zip')
+
+    with open(zip_path, 'wb') as file:
         file.write(response.content)
     print("Download completed.")
+
+    print("Extracting files...")
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        zip_ref.extractall('soundfonts')
+    print("Extraction completed.")
+    os.remove(zip_path)
 else:
     print(f"Failed to download file. Status code: {response.status_code}")
+
