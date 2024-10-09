3ad1307b290c9d212e32249707a2220d7ab47fd9
Edward <103079602+EddieN03@users.noreply.github.com>
2024-10-09 17:15:52 -0400

    Update VoiceCommand_test.py
    Should be a fix for the issue Nick was getting last week

--- module_tests/VoiceCommand_test.py
+++ module_tests/VoiceCommand_test.py
@@ -1,6 +1,6 @@
 import unittest
 from unittest.mock import Mock, patch, MagicMock
-from textTesting import classify_command
+#from textTesting import classify_command
 try:
     from src.VoiceCommandThread.py import VoiceAnalyzerThread
 except:
