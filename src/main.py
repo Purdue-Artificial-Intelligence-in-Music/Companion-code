70bf63a46413047a0a63126b31c8b9176ec0a420
Ryan Jordan <34822768+rtjord@users.noreply.github.com>
2024-10-10 16:57:15 -0400

    Ranav main integration (#97)
    * Update Synchronizer.py
    
    * Update main.py
    
    * Update main.py
    
    * fixed csv file containing alignment and accompaniment errors
    
    ---------
    
    Co-authored-by: rsethi18 <141153874+rsethi18@users.noreply.github.com>

--- src/main.py
+++ src/main.py
@@ -1,12 +1,20 @@
 from Synchronizer import Synchronizer
 import time
-import os
-from alignment_error import load_data, calculate_alignment_error
 import numpy as np
+from alignment_error import load_data, calculate_alignment_error
+from accompaniment_error import calculate_accompaniment_error
+import os
 
-reference = os.path.join('data', 'audio', 'mozart', 'synthesized', 'solo.wav')
-accompaniment = os.path.join('data', 'audio', 'mozart', 'synthesized', 'accompaniment.wav')
-source = os.path.join('data', 'audio', 'mozart', 'live', 'variable_tempo.wav')
+output_file = open('output\\output.txt', 'w')
+
+soloist_times = []
+predicted_times = []
+accompanist_times = []
+playback_rates = []
+
+reference = os.path.join('data', 'audio', 'bach', 'synthesized', 'solo.wav')
+accompaniment = os.path.join('data', 'audio', 'bach', 'synthesized', 'accompaniment.wav')
+source = os.path.join('data', 'audio', 'bach', 'live', 'variable_tempo.wav')
 
 # create a synchronizer object
 synchronizer = Synchronizer(reference=reference,
@@ -28,32 +36,44 @@ input('Press Enter to start the performance')
 # start the synchronizer
 synchronizer.start()
 
-# wait until the synchronizer is active
 while not synchronizer.is_active():
     time.sleep(0.01)
 
-print('Starting performance')
 try:
-    while synchronizer.update():  # update the playback rate of the accompaniment
+    while synchronizer.update():
         soloist_time = synchronizer.soloist_time()
         estimated_time = synchronizer.estimated_time()
         accompanist_time = synchronizer.accompanist_time()
         playback_rate = synchronizer.player.playback_rate
+
+        soloist_times.append(soloist_time)
+        predicted_times.append(estimated_time)
+        accompanist_times.append(accompanist_time)
+        playback_rates.append(playback_rate)
+
         print(f'Soloist time: {soloist_time:.2f}, Estimated time: {estimated_time:.2f}, Accompanist time: {accompanist_time:.2f}, Playback rate: {playback_rate:.2f}, Unread frames: {synchronizer.score_follower.mic.count:4}', end='\r')
-        # print(synchronizer.score_follower.mic.count)
-        alignment_error = estimated_time - soloist_time
-        accompaniment_error = accompanist_time - estimated_time
+        output_file.write(f'Soloist time: {soloist_time:.2f}, Predicted time: {estimated_time:.2f}, '
+                          f'Accompanist time: {accompanist_time:.2f}, Playback rate: {playback_rate:.2f}\n')
 
 except KeyboardInterrupt:
     synchronizer.stop()
 
 synchronizer.save_performance(path='performance.wav')
+output_file.close()
 
-# Measure the alignment error
 score_follower = synchronizer.score_follower
 
-df = load_data('data\\alignments\\variable_tempo.csv')
-warping_path = np.asarray(score_follower.path, dtype=np.float32)
+df_alignment = load_data('data\\alignments\\variable_tempo.csv')
+warping_path = np.array(synchronizer.get_warping_path(), dtype=np.float32)
 warping_path = warping_path * score_follower.win_length / score_follower.sample_rate
-df = calculate_alignment_error(df, warping_path)
-df.to_csv('output\\variable_tempo_log.csv', index=False)
+df_alignment = calculate_alignment_error(df_alignment, warping_path)
+
+estimated_times = warping_path[:, 0]
+df_accompaniment = calculate_accompaniment_error(
+    df_alignment,
+    estimated_times=predicted_times,
+    accompanist_times=np.asarray(accompanist_times)
+)
+
+df_accompaniment.to_csv('output\\error_analysis_per_measure_variable.csv', index=False)
+print(df_accompaniment)
