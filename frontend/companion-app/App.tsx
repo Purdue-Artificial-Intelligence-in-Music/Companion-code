f2c5f760feecf1ee96269134a28dafa7137276e2
Ryan Jordan <34822768+rtjord@users.noreply.github.com>
2024-10-23 15:35:05 -0400

    Revert "Sending the latest - autoscroll and timestamp-based motion (#101)" (#104)
    This reverts commit 5d022cf23fd5257b16e286a686bba0aaa6f81206.

--- frontend/companion-app/App.tsx
+++ frontend/companion-app/App.tsx
@@ -3,7 +3,7 @@ import { StatusBar } from 'expo-status-bar'; // Manages the status bar on mobile
 import { StyleSheet, Text, View, SafeAreaView, Pressable } from 'react-native'; // Imports styling and layout components
 import React, { MutableRefObject, useEffect, useRef, useState } from 'react'; // Imports React and hooks
 import { OpenSheetMusicDisplay, Cursor } from 'opensheetmusicdisplay'; // Imports the OpenSheetMusicDisplay library for rendering sheet music
-import { Play_Button, Score_Select, Stop_Button, TimeStampBox } from './assets/Components';
+import { Play_Button, Next_Button, Score_Select, RenderSomethingButton } from './Components';
 
 // Define the main application component
 export default function App() {
@@ -21,13 +21,10 @@ export default function App() {
   // Create a state representing the file name of the current piece
   const [score, setScore] = useState("air_on_the_g_string.musicxml");
   // Create a state holding the cursor (a ref didn't work)
-  const cursorRef = useRef<Cursor | null>(null);
+  const cursor = useRef<Cursor | null>(null);
   const osdRef = useRef<OpenSheetMusicDisplay | null>(null);
   // And one determining whether the piece is currently playing
   const [playing, setPlaying] = useState(false);
-  
-  const [cursorPos, setCursorPos] = useState<number>(-1); // this and all features using it are to be deprecated
-  const [timestamp, setTimestamp] = useState<string>("0.0")
     
   const handleFileUpload = (file: File) => {
     const reader = new FileReader();
@@ -43,7 +40,6 @@ export default function App() {
     };
     reader.readAsText(file);
   };
-
   // useEffect hook to handle side effects (like loading music) after the component mounts
   // and when a piece is selected
   useEffect(() => {
@@ -62,81 +58,40 @@ export default function App() {
           autoResize: false, // Enable automatic resizing of the sheet music display
         });
 
-    // Create an instance of OpenSheetMusicDisplay, passing the reference to the container
-    const osm = new OpenSheetMusicDisplay(osmContainerRef.current as HTMLElement, {
-      autoResize: true, // Enable automatic resizing of the sheet music display
-      followCursor: true, // And follow the cursor
-    });
+        osdRef.current = osm;
 
-    osdRef.current = osm;
-
-    // Load the music XML file from the assets folder
-    osm.load('assets/' + score)
-      .then(() => {
-        // Render the sheet music once it's loaded
-        osm.render();
-        console.log('Music XML loaded successfully'); // Log success message to console
-        cursorRef.current = osm.cursor; // Pass reference to cursor
-        cursorRef.current.CursorOptions = { ...cursorRef.current.CursorOptions, ... { "follow": true} }
-    })
-      .catch((error) => {
-        // Handle errors in loading the music XML file
-        console.error('Error loading music XML:', error); // Log the error message
-      });
+        // Load the music XML file from the assets folder
+        osm.load('assets/' + score)
+          .then(() => {
+            // Render the sheet music once it's loaded
+            osm.render();
+            console.log('Music XML loaded successfully'); // Log success message to console
+            cursor.current = osm.cursor; // Pass reference to cursor
+        })
+          .catch((error) => {
+            // Handle errors in loading the music XML file
+            console.error('Error loading music XML:', error); // Log the error message
+          });
+      }
+    }
+    
 
     // Cleanup function to dispose of the OpenSheetMusicDisplay instance if needed
     return () => {
     };
-  }, [score]); // Dependency array means this effect runs once when the component mounts and again when a new score is selected
-
-  // useEffect to update the time-stamp
-  useEffect( () => {
-    // move the cursor based on the timestamp!
-    if (cursorRef.current?.hidden) cursorRef.current?.show();
-        var ts = Number(timestamp); // the timestamp is a string rn bc it's in an input box
-        var ct = cursorRef.current?.Iterator.CurrentSourceTimestamp.RealValue; // cursor time
-        var nct; // new cursor time
-        var cpos = cursorPos; // pointless? update to cursor state
-        if (ct !== undefined) { // if the cursor does exist...
-            while (ct < ts && !(cursorRef.current?.Iterator.EndReached)) {
-                // cursorRef.current?.Iterator.moveToNext()
-                cursorRef.current?.next(); // Move it until we pass the timestamp (or reach the end)
-                cpos += 1;
-                nct = cursorRef.current?.Iterator.CurrentSourceTimestamp.RealValue
-                if (nct !== undefined) ct = nct;
-            }
-            if (!(cursorRef.current?.Iterator.EndReached)) {
-                // cursorRef.current?.Iterator.moveToPrevious()
-                cursorRef.current?.previous(); // Then if we haven't reached the end, move back
-                setCursorPos(cpos - 1);
-            }
-        }
-        if (osdRef.current !== undefined && osdRef.current?.Sheet !== undefined) {
-          osdRef.current.render();
-        }
-  }, [timestamp])
-
-
+  }, [score, scores]); // Dependency array means this effect runs once when the component mounts and again when a new score is selected
+  
   // Render the component's UI
   return (
-    <SafeAreaView style={styles.container}>{/* Provides safe area insets for mobile devices */}
-      <Text style={styles.title}>Companion, the digital accompanist</Text>
-      <Score_Select score={score} scoreOptions={scores} setScore={setScore}/>
-      <View style={styles.button_wrapper}>
-        <Play_Button my_cursor={cursorRef} playing={playing} setPlaying={setPlaying}
-        button_style={styles.button} text_style={styles.button_text}
-        cursorPos={cursorPos} setCursorPos={setCursorPos} osdRef={osdRef}
-        />
-        <Stop_Button setPlaying={setPlaying} button_style={styles.button} text_style={styles.button_text}/>
-        <TimeStampBox timestamp={timestamp} setTimestamp={setTimestamp} style={styles.text_input}/>
-      </View>
-      {/* <Pressable onPress={ () => { cursorRef.current?.reset() } }><Text>RESET CURSOR</Text></Pressable> */}
+    <SafeAreaView style={styles.container}> {/* Provides safe area insets for mobile devices */}
+      <Text style={styles.title}>Companion, the digital accompanist</Text> {/* Title text */}
+      <Score_Select scores={scores} setScore={setScore} onFileUpload={handleFileUpload}/>
+      <Play_Button my_cursor={cursor} playing={playing} setPlaying={setPlaying}/>
+      <Next_Button my_cursor={cursor}/>
       <div style={styles.scrollContainer}> {/* Container for scrolling the sheet music */}
-        <Text>Cursor position (used by deprecated play button): {cursorPos}</Text>
-        <div ref={osmContainerRef} style={styles.osmContainer}>
-          </div> Reference to the SVG container for sheet music
+        <div ref={osmContainerRef} style={styles.osmContainer}></div> {/* Reference to the SVG container for sheet music */}
       </div>
-      <StatusBar style="auto" />{/* Automatically adjusts the status bar style */}
+      <StatusBar style="auto" /> {/* Automatically adjusts the status bar style */}
     </SafeAreaView>
   );
 }
@@ -168,27 +123,4 @@ const styles = StyleSheet.create({
     borderColor: 'black', // Set border color to black
     overflow: 'hidden', // Ensure content doesn't overflow outside this container
   },
-  button: {
-    flex: 0.2,
-    borderColor: 'black',
-    borderRadius: 15,
-    backgroundColor: 'lightblue',
-  },
-  button_text: {
-    fontSize: 20,
-    textAlign: 'center'
-  },
-  button_wrapper: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    padding: 10,
-    backgroundColor: 'lightgray',
-    width: '100%',
-    minHeight: 50
-  },
-  text_input: {
-    backgroundColor: 'white',
-    flex: 0.3
-  }
 });
