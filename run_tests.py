c386552dbf013882eba3b300c8c1ec449506514b
Nick <ko.binghong@gmail.com>
2024-10-28 15:30:03 -0400

    update run_tests script

--- run_tests.py
+++ run_tests.py
@@ -11,6 +11,11 @@ def display_tests(tests):
             print("  " + test + ",")
     print("]")
 
+def set_verbose_flag() -> str: 
+    user_input = input("do you want the verbose flag running with your tests?: [y]es or [n]o\n")
+    flag = "-v" if user_input == "y" else ""
+    return flag
+
 if __name__ == '__main__':
     argc = len(sys.argv)
 
@@ -19,8 +24,10 @@ if __name__ == '__main__':
 
     if argc == 2:
         test_name = sys.argv[1]
+        flag = set_verbose_flag()
+
         if os.path.exists('module_tests/{}'.format(test_name)):
-            os.system("python -m unittest module_tests/{} -v".format(test_name))
+            os.system("python -m unittest module_tests/{} {}".format(test_name, flag))
         else: 
             print("Test {} does not exist in the module_tests/ directory".format(test_name))
             print("please choose from the following tests:")
@@ -28,10 +35,13 @@ if __name__ == '__main__':
             print("or run all tests by providing no arguments")
             print("or write a test and put it in the module_tests/ directory")
             print("note: test names should follow the form: <name>_test.py'")
+
     elif argc == 1:
+        flag = set_verbose_flag()
         print("running a total of {} tests".format(len(tests)))
+
         for test in tests:
-            os.system("python -m unittest module_tests/{} -v".format(test))
+            os.system("python -m unittest module_tests/{} {}".format(test, flag))
     else:
         print("Invalid number of arguments: provide either a specific test or no test")
         exit(1)
