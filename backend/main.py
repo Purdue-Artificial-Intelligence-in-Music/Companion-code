5165143efa76b7af3d3d2b950407469542f596c1
Ryan Jordan <rtjord@gmail.com>
2025-02-12 15:53:21 -0500

    updated requirements.txt

--- backend/main.py
+++ backend/main.py
@@ -19,15 +19,15 @@ SAMPLE_RATE = 44100  # Universal sample rate
 CHANNELS = 1  # Universal number of channels
 WIN_LENGTH = 4096  # Samples per window for score follower
 HOP_LENGTH = 4096  # Samples per hop for score follower. This should be the same as WIN_LENGTH for now. Can add support for different values in the future
-C = 100  # Search width for score follower. Higher values are more computationally expensive
+C = 50  # Search width for score follower. Higher values are more computationally expensive
 MAX_RUN_COUNT = 3  # Slope constraint for score follower. 1 / MAX_RUN_COUNT <= slope <= MAX_RUN_COUNT
 DIAG_WEIGHT = 0.5  # Weight for the diagonal in the cost matrix for score follower. Values less than 2 bias toward diagonal steps
 MAX_DURATION = 600  # Maximum duration of audio buffer in seconds
 
 
-TEMPO = 180  # Tempo at which piece is played in BPM
+TEMPO = 150  # Tempo at which piece is played in BPM
 PIECE_NAME = 'twinkle_twinkle'  # Name of piece
-PROGAM_NUMBER = 43  # Program number for accompaniment instrument
+PROGAM_NUMBER = 0  # Program number for accompaniment instrument
 SOLO_VOLUME_MULTIPLIER = 1
 
 MIDI_SCORE = os.path.join('data', 'midi', PIECE_NAME + '.mid')  # Path to MIDI file
@@ -37,7 +37,7 @@ generator = AudioGenerator(score_path=MIDI_SCORE)  # Create an AudioGenerator in
 generator.generate_audio(output_dir=OUTPUT_DIR, tempo=TEMPO)  # Generate a WAV file for each instrument in the MIDI file
 
 reference = os.path.join('data', 'audio', PIECE_NAME, 'instrument_0.wav')  # Path to reference audio file
-source = os.path.join('data', 'audio', PIECE_NAME, 'instrument_0.wav')  # Path to soloist audio file (can optionally replace mic input)
+source = os.path.join('data', 'audio', PIECE_NAME, '200bpm', 'instrument_0.wav')  # Path to soloist audio file (can optionally replace mic input)
 
 source_audio, _ = librosa.load(source, sr=SAMPLE_RATE)  # load soloist audio
 source_audio = source_audio.reshape((CHANNELS, -1))  # reshape soloist audio to 2D array
@@ -77,7 +77,7 @@ def callback(in_data, frame_count, time_info, status):
     soloist_times.append(source_index / SAMPLE_RATE)  # log soloist time for error analysis
     estimated_times.append(estimated_time)  # log estimated time for error analysis
 
-    position = estimated_time  / 60 * performance.current_tempo  # convert estimated time (seconds) to position in piece (beats)
+    position = estimated_time  / 60 * TEMPO  # convert estimated time (seconds) to position in piece (beats)
 
     # Tell the MidiPerformance instance to update the score position.
     # The MidiPerformance instance will play the most recently passed note in the accompaniment.
