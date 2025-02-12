47c0a74076cceaa796b9abf7a14835fc827c14ca
Ryan Jordan <34822768+rtjord@users.noreply.github.com>
2025-02-12 16:13:54 -0500

    Updated requirements.txt (#178)
    * updated requirements.txt
    
    * updated requirements.txt and added support for different reference and source tempos
    
    * removed music21 from requirements.txt

--- backend/src/audio_generator.py
+++ backend/src/audio_generator.py
@@ -186,10 +186,10 @@ class AudioGenerator:
 if __name__ == '__main__':
     # Example usage:
     base_dir = Path(__file__).parent
-    score = os.path.join('data', 'midi', 'mozart_k487_no2.mid')
-    output_dir = os.path.join('data', 'audio', 'mozart_k487_no2')
+    score = os.path.join('data', 'midi', 'twinkle_twinkle.mid')
+    output_dir = os.path.join('data', 'audio', 'twinkle_twinkle', '200bpm')
     SAMPLE_RATE = 44100
-    TEMPO = 60
+    TEMPO = 200
 
     try:
         generator = AudioGenerator(score_path=score)
