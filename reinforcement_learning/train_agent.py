23e7a90b8dde24729aac869fb740e4cb53d757b1
Brian Qi <89670897+qi116@users.noreply.github.com>
2025-04-30 13:35:54 -0400

    Score Following RL (#195)
    * Updated rl requirements.txt
    
    * Trying to change environment. Changes: Increased window to 15 when training. -0.5 reward when standing still to encourage movement. Tried randomizing start location for training
    
    * Changed columns_per_beat to 16, basically manually aligning so that agent can keep moving forwards.
    
    * Added vecnormalization stuff. Seems to work well. Changed cols_per_beat to 4
    
    * Included changes for normalization. Might need to change tracking_window.
    
    ---------
    
    Co-authored-by: Brian Qi <brianqi@Brians-MacBook-Pro.local>

--- reinforcement_learning/train_agent.py
+++ reinforcement_learning/train_agent.py
@@ -2,6 +2,7 @@ import numpy as np
 from stable_baselines3 import PPO
 from gymnasium_env.envs.score_following_env import ScoreFollowingEnv
 from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.vec_env import VecNormalize
 from tqdm import tqdm
 
 
@@ -17,8 +18,11 @@ vec_env = make_vec_env(
         "audio_path": "ode_beg.mp3",
         "bpm": 70,
         "alignment": alignment,
+        "training": True
     },
 )
+
+vec_env = VecNormalize(vec_env, norm_obs=True, norm_reward=False, gamma=0.99)
 
 # Create the PPO model using MultiInputPolicy to handle the Dict observation space.
 model = PPO("MultiInputPolicy", vec_env, verbose=1)
@@ -27,4 +31,6 @@ model = PPO("MultiInputPolicy", vec_env, verbose=1)
 model.learn(total_timesteps=100_000, progress_bar=tqdm)
 
 # Save the trained model.
-model.save("ppo_score_following")
+model.save("ppo_score_following4")
+vec_env.save("ppo_score_following_env4")
+vec_env.close()
